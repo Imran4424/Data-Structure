39ead8bdb1fa6e5f92b3cc2b4f36d08bca384a7b
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-03-13 16:15:08 +0600

    graph codes

---
+++ Linked List/LAB/Link_List.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+struct node{
+    int info;
+    node* link;
+};
+//typedef struct node *nodeptr;
+
+node* provide()
+{
+    node *q = new node;
+
+    return q;
+}
+
+node* insertnode (node *ptr, int val){
+	node *q;
+	q = provide();
+	
+	q->info = val;
+	ptr -> link = q;
+	return q;
+}
+
+int main()
+{
+    node *start,*ptr;
+
+    int indicator,val,count;
+    
+    start = provide();
+    ptr = start;
+	
+	cout<<"Enter a negative value to terminate"<<endl;
+	
+    while(cin>>val){
+        if(val<0){
+            break;
+        }
+        
+        ptr = insertnode(ptr,val);
+    }
+    ptr = start;
+
+    while(ptr!=0){
+		cout<<ptr->info<<endl;
+		ptr = ptr->link;
+    }
+    
+    return 0;
+}
