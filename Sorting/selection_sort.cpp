d6f6a3013042aca0c1941a1f5600da7dff0b9e15
Imran4424 <akashganga.programming.534363@gmail.com>
2018-07-14 19:43:13 +0600

    data structures

---
+++ Sorting/selection_sort.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+
+int main(int argc, char const *argv[])
+{
+	cout<<"enter the size of the array"<<end;
+	int n;
+	cin>>n;
+
+	int ar[n];
+
+	cout<<"enter the array elements"<<endl;
+
+	
+
+
+	return 0;
+}
