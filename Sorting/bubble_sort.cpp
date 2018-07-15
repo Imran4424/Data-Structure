07aa3a092d2d2a33b834628cf3641a1b63b8460c
Imran4424 <akashganga.programming.534363@gmail.com>
2018-07-16 03:32:44 +0600

    data structures

--- Sorting/bubble_sort.cpp
+++ Sorting/bubble_sort.cpp
@@ -4,8 +4,12 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-	
+	cout<<"enter the size of the array"<<endl;
+	cin>>n;
+	int ar[n];
 
-	
+	cout<<"enter the array elements"<<endl;
+
+
 	return 0;
 }
