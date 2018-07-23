3a765ed94bac37875921e4bddec391b4a79eedac
Imran4424 <akashganga.programming.534363@gmail.com>
2018-07-24 05:05:33 +0600

    data structures

--- Sorting/bubble_sort.cpp
+++ Sorting/bubble_sort.cpp
@@ -29,7 +29,19 @@ int* bubble_sort(int ar[],int size)
 
 void bubble_sort_decending(int ar[], int size)
 {
+	for (int i = 0; i < size-1; ++i)
+	{
+		for(int j = 0; j < size-1-i; ++j)
+		{
+			if (ar[j] < ar[j+1])
+			{
+				int temp = ar[j];
+				ar[j] = ar[j+1];
+				ar[j+1] = temp;
+			}
+		}
 
+	}
 }
 
 
