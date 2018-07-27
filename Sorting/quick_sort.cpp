fa39f2f593b3e046b3075d51775a1aa40d6652f0
Imran4424 <akashganga.programming.534363@gmail.com>
2018-07-27 14:32:29 +0600

    sorting

--- Sorting/quick_sort.cpp
+++ Sorting/quick_sort.cpp
@@ -8,7 +8,14 @@ int main(int argc, char const *argv[])
 	int n;
 	cin>>n;
 
-	
+	int ar[n];
+
+	cout<<"enter the array elements"<<endl;
+
+	for (int i = 0; i < n; ++i)
+	{
+		cin>>ar[i];
+	}
 
 
 	return 0;
