0e216e0af09de9a77fe72dcacef48715a35afafa
Imran4424 <akashganga.programming.534363@gmail.com>
2018-07-27 07:47:43 +0600

    data structures

--- Sorting/insertion_sort.cpp
+++ Sorting/insertion_sort.cpp
@@ -33,7 +33,23 @@ void insertion_sort(int ar[], int size)
 
 void insertion_sort_decending(int ar[], int size)
 {
-	
+	for (int i = 1; i < size; ++i)
+	{
+		int hole_pos = i;
+		
+
+		for(int j = i-1; j >= 0; --j)
+		{
+			if (ar[j] < ar[hole_pos])
+			{
+				int temp = ar[j];
+				ar[j] = ar[hole_pos];
+				ar[hole_pos] = temp;
+
+				hole_pos = j;
+			}
+		}
+	}
 }
 
 int main(int argc, char const *argv[])
