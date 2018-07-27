2e8429d6fbd7f396564176e80c32243b5fbee32e
Imran4424 <akashganga.programming.534363@gmail.com>
2018-07-27 10:24:53 +0600

    sorting

--- Sorting/merge_sort.cpp
+++ Sorting/merge_sort.cpp
@@ -4,6 +4,13 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-	/* code */
+	cout<<"enter the array size"<<endl;
+	int n;
+	cin>>n;
+
+	int ar[n];
+
+	cout<<"enter the array elements"<<endl;
+
 	return 0;
 }
