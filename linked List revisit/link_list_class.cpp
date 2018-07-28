26cd6dedcaaab458b1e2a934201f38c7c257465c
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-07-28 17:54:05 +0600

    linked lists

--- linked List revisit/link_list_class.cpp
+++ linked List revisit/link_list_class.cpp
@@ -8,8 +8,53 @@ public:
 	{
 		int data;
 		node* nextAddress;
-	};	
+	};
+
+	node* head;
+
+	Link_list()
+	{
+		head = NULL;
+	}
+
+	void insert_node_at_the_end(int value)
+	{
+		node* temp = new node();
+
+		temp -> data = value;
+		temp -> nextAddress = NULL;
+
+		if (head == NULL)
+		{
+			head = temp;
+
+			return;
+		}
+
+		node* travel = head;
+
+		while(travel -> nextAddress != NULL)
+		{
+			travel = travel -> nextAddress;
+		}
+
+		travel -> nextAddress = temp;
+	}
+
+	void insert_node_at_the_begin(int value)
+	{
+		node* temp = new node();
+
+		temp -> data = value;
+		temp -> nextAddress = head;
+
+		head = temp;
+	}
+
+
 };
+
+
 
 
 int main(int argc, char const *argv[])
