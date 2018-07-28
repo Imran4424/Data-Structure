7b98203ab95a059abefe5d6c5d5b585023b9a482
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-07-28 17:21:57 +0600

    linked lists

--- linked List revisit/simple.cpp
+++ linked List revisit/simple.cpp
@@ -206,7 +206,8 @@ int main(int argc, char const *argv[])
 	for (int i = 0; i < num; ++i)
 	{
 		//delete_at_the_end();
-		delete_at_the_begin();
+		//delete_at_the_begin();
+		delete_at_nth_position(8);
 	}
 
 	// display again
