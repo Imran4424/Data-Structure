f8cae7a198e9360c19e252ebbe71a8afe27d1965
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-07-30 04:19:40 +0600

    linked list - Stack - Queue

--- linked List revisit/DoublyLinkList.cpp
+++ linked List revisit/DoublyLinkList.cpp
@@ -1,2 +1,19 @@
 #include <iostream>
 using namespace std;
+
+class DoublyLinkList
+{
+	public: struct node
+	{
+		int data;
+		node* prevAddress;
+		node* nextAddress;
+	};
+
+	public: DoublyLinkList()
+	{
+
+	}
+
+	
+};
