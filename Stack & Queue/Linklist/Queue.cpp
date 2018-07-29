f5c36de563082be888b836417b705ef2d0785bb5
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-07-29 07:58:00 +0600

    linked lists

--- Stack & Queue/Linklist/Queue.cpp
+++ Stack & Queue/Linklist/Queue.cpp
@@ -2,3 +2,73 @@
 using namespace std;
 
 
+class Queue
+{
+		private: struct node
+	{
+		int data;
+		node* nextAddress;
+	};
+
+	private: node* head;
+
+	public: Stack()
+	{
+		head = NULL;
+	}
+
+	public: bool IsEmpty()
+	{
+		if (head == NULL)
+		{
+			return true;
+		}
+
+		return false;
+	}
+
+	public: int size()
+	{
+		if (head == NULL)
+		{
+			return 0;
+		}
+
+		int count = 0;
+
+		node* travel = head;
+
+		while(travel != NULL)
+		{
+			travel = travel -> nextAddress;
+
+			count++;
+		}
+
+		return count;	
+	}
+
+	public: void clear()
+	{
+		head = NULL;
+	}
+
+	public: void push(int value)
+	{
+		node* temp = new node();
+
+		temp -> data = value;
+		temp -> nextAddress = NULL;
+
+		node* travel = head;
+
+		while(travel -> nextAddress != NULL)
+		{
+			travel = travel -> nextAddress;
+		}
+
+		travel -> nextAddress = temp;
+	}
+
+	
+};
