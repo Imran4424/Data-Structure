737377f927a7e18d37c441a5c163b55056ec0601
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-07-31 23:20:22 +0600

    tree

--- linked List revisit/Generic/Generic_Practice.cpp
+++ linked List revisit/Generic/Generic_Practice.cpp
@@ -14,6 +14,9 @@ T Sum(T a, T b)
 int main(int argc, char const *argv[])
 {
 	cout<<"Sum is: "<<Sum(5,7)<<endl;
+	cout<<"Sum is: "<<Sum(5.5, 10.45)<<endl;
+	cout<<"Sum is: "<<Sum('a', 'c')<<endl;
+	cout<<"Sum is: "<<Sum("I am a ","Programmer")<<endl;
 
 	return 0;
 }
