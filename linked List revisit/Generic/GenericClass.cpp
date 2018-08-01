a7906113659fe4233414b64b3a5739fccb4e0c2c
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-08-01 08:16:11 +0600

    tree

--- linked List revisit/Generic/GenericClass.cpp
+++ linked List revisit/Generic/GenericClass.cpp
@@ -4,11 +4,11 @@ using namespace std;
 template <class T>
 class GenericClass
 {
-	private: T first;
+	private: T first,second;
 	
-	public: GenericClass()
+	public: GenericClass(T a, T b)
 	{
-		
+
 	}
 	
 };
