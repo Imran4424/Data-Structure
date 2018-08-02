ac1ef6c5e5642dc3781c7edede5e68fb8cecf4e1
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-08-02 22:11:57 +0600

    tree

--- Tree revisit/LOJ_1082_Array_Queries.cpp
+++ Tree revisit/LOJ_1082_Array_Queries.cpp
@@ -6,9 +6,12 @@ const int maxx = 100001;
 int ar[maxx] = {0};
 int tree[3*maxx] = {0};
 
-void BuidTree()
+void BuidTree(int node, int begin, int end)
 {
-	
+	if (begin == end)
+	{
+		tree[node] = ar[begin];
+	}
 }
 
 int main(int argc, char const *argv[])
