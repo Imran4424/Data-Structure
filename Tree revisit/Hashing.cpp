07b4c8f87d7f2cba5e323d3f8d391b50aeb3e279
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-08-02 07:45:29 +0600

    tree

--- Tree revisit/Hashing.cpp
+++ Tree revisit/Hashing.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
-using  namespace std;
+using namespace std;
 
 int HashCode(int number)
 {
 	return number % 20;
 }
+
+struct HashTable
+{
+	int key;
+	int number;
+	string name;
+};
 
 
 int main(int argc, char const *argv[])
