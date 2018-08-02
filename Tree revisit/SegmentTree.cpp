2f6dfeb58e56788b25964847668e3a7b668d96c0
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-08-02 13:06:07 +0600

    tree

--- Tree revisit/SegmentTree.cpp
+++ Tree revisit/SegmentTree.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
 using namespace std;
 
+const int maxx = 1000001;
 
+int ar[maxx] = {0};
+int tree[3*maxx] = {0};
 
 
 int main(int argc, char const *argv[])
 {
+	cout<<"enter the array size"<<endl;
+
 	
+
 	return 0;
 }
