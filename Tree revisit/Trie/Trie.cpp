56ade7b5cbe289304b9a2d99e2f95faf694141af
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-08-03 17:29:24 +0600

    tree

--- Tree revisit/Trie/Trie.cpp
+++ Tree revisit/Trie/Trie.cpp
@@ -4,7 +4,21 @@ using namespace std;
 
 class Trie
 {
-	
+	private: struct node
+	{
+		bool endmark;
+		node* next[26+1];
+
+		node()
+		{
+			endmark = false;
+
+			for (int i = 0; i < 27; ++i)
+			{
+				next[i] = NULL;
+			}
+		}
+	};
 	
 };
 
