e40fe4f4f9669aca6bd6736fc205869fadfec24f
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-08-30 15:32:26 +0600

    pair

---
+++ STL/Pair/nestedPairPrQueueAsc.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <utility>
+#include <queue>
+#include <vector>
+using namespace std;
+
+typedef pair< int, pair<int, int> > nestedCouple;
+
+int main(int argc, char const *argv[])
+{
+	priority_queue < nestedCouple > weightedList;
+
+	cout << "How many edges?" << endl;
+	int test;
+	cin>> test;
+
+	int source, destination, weight;
+
+	cout << "enter the source, destination and weight" << endl;
+
+	while(test--)
+	{
+		cin >> source >> destination >> weight;
+
+		weightedList.push(make_pair(weight, make_pair(source, destination)));
+	}
+
+	cout << "pairs in priority_queue are" << endl;
+
+	while(!weightedList.empty())
+	{
+		nestedCouple hand = weightedList.top();
+		weightedList.pop();
+
+		cout << hand.first << " " << hand.second.first << " " << hand.second.second << endl;
+	}
+
+
+	return 0;
+}
