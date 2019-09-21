b9fc73bebc07079204d0fe3c0d67b375f8467d1d
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-09-21 08:18:56 +0600

    linked list

--- Tree/BST/BSTCreation.cpp
+++ Tree/BST/BSTCreation.cpp
@@ -3,5 +3,17 @@ using namespace std;
 
 struct node
 {
-	
+	int data;
+	node* left;
+	node* right;
+
+	node() {} // default constructor
+
+	node(int value)
+	{
+		data = value;
+		left = NULL;
+		right = NULL;
+	}
 };
+
