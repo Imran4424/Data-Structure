f4b34bb6286e34ac78bb3cbad433dca6603c784d
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-01-03 02:10:49 +0600

    tree

---
+++ PalindromeChecking.cpp
@@ -0,0 +1,33 @@
+// palindrome checking
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int number;
+    cin >> number;
+    
+    int normal = 0, reverse = 0, powerCount = 1;
+    
+    while(0 != number) {
+        
+        int digit = number % 10;
+        
+        reverse = reverse * 10 + digit;
+        
+        normal = digit * powerCount + normal;
+        
+        powerCount = powerCount * 10;
+        
+        number = number /10;
+    }
+    
+    cout << normal << " " << reverse << endl;
+    
+    if(normal == reverse) {
+        cout << "Palindrome" << endl;
+    } else {
+        cout << "Not Palindrome" << endl;
+    }
+}
