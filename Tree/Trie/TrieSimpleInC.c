d3da4cb2952bd6d5f8ca1a824b80fe3f5cb80dff
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-02-13 01:18:35 +0600

    trie

--- Tree/Trie/TrieSimpleInC.c
+++ Tree/Trie/TrieSimpleInC.c
@@ -15,9 +15,27 @@ void initTrieNode(node* current) {
 	}
 }
 
+void trieInsertion(char *word) {
+    node* travel = root;
+
+    for(int i = 0; word[i]; i++) {
+        int letter = word[i] - 'a';
+
+        if(NULL == travel -> next[letter]) {
+            travel -> next[letter] = malloc(sizeof(node));
+            initTrieNode(travel -> next[letter]);
+        }
+        travel = travel -> next[letter];
+    }
+    /// marking the letters end point
+    travel -> endMark = true;
+}
+
 int main(int argc, char const *argv[])
 {
 	node* root = malloc(sizeof(node));
+	initTrieNode(root);
+
 
 	return 0;
 }
