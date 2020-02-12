9ba5dfdb5730840a183eec2ccce0a12f6a6b3aca
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-02-13 01:15:56 +0600

    trie

--- Tree/Trie/TrieSimpleInC.c
+++ Tree/Trie/TrieSimpleInC.c
@@ -8,8 +8,16 @@ typedef struct node {
     struct node *next[26];
 } node;
 
+void initTrieNode(node* current) {
+	current -> endMark = false;
+	for(int i = 0; i < 26; i++) {
+		current -> next[i] = NULL;
+	}
+}
+
 int main(int argc, char const *argv[])
 {
 	node* root = malloc(sizeof(node));
+
 	return 0;
 }
