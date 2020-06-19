9fcd59fdfdab606d631ebd0d54e4cf39996c4393
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-06-19 21:53:41 +0600

    segment tree

--- Tree/Segment Tree/SegmentTree.cpp
+++ Tree/Segment Tree/SegmentTree.cpp
@@ -38,6 +38,9 @@ int query(int node, int startIndex, int endIndex, int startRange, int endRange)
 		return tree[node];
 	}
 
+	// partially inside the query range
+	// so we need to break down into little pieces
+
 	int left = node*2;
 	int right = node*2 + 1;
 
