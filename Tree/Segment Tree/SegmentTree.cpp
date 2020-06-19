025852174b4112b342c84253eb023874fa56e700
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-06-19 19:37:26 +0600

    segment tree

--- Tree/Segment Tree/SegmentTree.cpp
+++ Tree/Segment Tree/SegmentTree.cpp
@@ -6,9 +6,9 @@ const int maxx = 1000001;
 int ar[maxx] = {0};
 int tree[3*maxx] = {0};
 
-void bulidTree(int node, int firstIndex, int end)
+void bulidTree(int node, int firstIndex, int lastIndex)
 {
-	if (firstIndex == end)
+	if (firstIndex == lastIndex)
 	{
 		tree[node] = ar[firstIndex];
 
@@ -21,19 +21,19 @@ void bulidTree(int node, int firstIndex, int end)
 	int mid = (left+right) / 2;
 
 	bulidTree(left,firstIndex,mid);
-	bulidTree(right,mid+1,end);
+	bulidTree(right,mid+1,lastIndex);
 
 	tree[node] = tree[left] + tree[right];
 }
 
-int Query(int node, int firstIndex, int end, int start_index, int end_index)
+int Query(int node, int firstIndex, int lastIndex, int start_index, int lastIndex_index)
 {
-	if (firstIndex > end_index || end < start_index)
+	if (firstIndex > lastIndex_index || lastIndex < start_index)
 	{
 		return 0;
 	}
 
-	if (firstIndex >= start_index && end <= end_index)
+	if (firstIndex >= start_index && lastIndex <= lastIndex_index)
 	{
 		return tree[node];
 	}
@@ -41,22 +41,22 @@ int Query(int node, int firstIndex, int end, int start_index, int end_index)
 	int left = node*2;
 	int right = node*2 + 1;
 
-	int mid = (firstIndex+end) / 2;
+	int mid = (firstIndex+lastIndex) / 2;
 
-	int left_sum = Query(left, firstIndex, mid, start_index, end_index);
-	int right_sum = Query(right, mid+1, end, start_index, end_index);
+	int left_sum = Query(left, firstIndex, mid, start_index, lastIndex_index);
+	int right_sum = Query(right, mid+1, lastIndex, start_index, lastIndex_index);
 
 	return left_sum + right_sum;
 }
 
-void Update(int node, int firstIndex, int end, int index, int new_value)
+void Update(int node, int firstIndex, int lastIndex, int index, int new_value)
 {
-	if (index > end || index < firstIndex)
+	if (index > lastIndex || index < firstIndex)
 	{
 		return;
 	}
 
-	if (firstIndex == index && firstIndex == end)
+	if (firstIndex == index && firstIndex == lastIndex)
 	{
 		tree[node] = new_value;
 
@@ -69,7 +69,7 @@ void Update(int node, int firstIndex, int end, int index, int new_value)
 	int mid = (left+right) / 2;
 
 	Update(left, firstIndex, mid, index, new_value);
-	Update(right, mid+1, end, index, new_value);
+	Update(right, mid+1, lastIndex, index, new_value);
 
 	tree[node] = tree[left] + tree[right];
 }
