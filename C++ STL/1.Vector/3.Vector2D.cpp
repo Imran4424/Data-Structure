4af05f22a360e0d088282025da6656346828c587
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2021-10-01 20:51:17 +0600

    stl

--- C++ STL/1.Vector/3.Vector2D.cpp
+++ C++ STL/1.Vector/3.Vector2D.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <stdlib.h>
+#include <time.h>
 using namespace std;
 
-void display(vector<vector<int>> arr) {
+void display(vector< vector<int> > arr) {
 	for (int i = 0; i < arr.size(); i++) {
 		for (int j = 0; j < arr[i].size(); j++) {
 			cout << arr[i][j] << " ";
@@ -17,7 +19,7 @@ void display(vector<vector<int>> arr) {
 int main(int argc, char const *argv[])
 {
 	// declaring 2D dynamic vector
-	vector<vector<int>> arr;
+	vector< vector<int> > arr;
 
 	vector<int> rowOne(5, 25);
 	vector<int> rowTwo(3, 15);
@@ -28,7 +30,39 @@ int main(int argc, char const *argv[])
 	arr.push_back(rowThree);
 
 	display(arr);
-	
+
+	// declaring fixed row sized 2D vector
+	vector< vector<int> > fixedRowArr(3);
+	// or below code
+	// vector<int> fixedRowArr[3];
+	fixedRowArr[0] = rowOne;
+	fixedRowArr[1] = rowTwo;
+	fixedRowArr[2] = rowThree;
+	display(fixedRowArr);
+
+	// declaring fixed row and col sized 2D vector
+	vector< vector<int> > fixedRowColArr(3, vector<int>(3));
+
+	// another way
+	// vector <int> fixedRowColArr[3];
+	// for (int i = 0; i < fixedRowColArr.size(); i++) {
+	// 	fixedRowColArr[i] = vector<int> (3);
+	// }
+
+	srand(time(0));
+	for (int i = 0; i < fixedRowColArr.size(); i++) {
+		for (int j = 0; j < fixedRowColArr[i].size(); j++) {
+			fixedRowColArr[i][j] = rand() % 100 + 10;
+		}
+	}
+
+	display(fixedRowColArr);
+
+	// declaring fixed row and col sized 2D vector with value
+	vector< vector<int> > fixedRowColArrValue(3, vector<int>(3, 13));
+
+	display(fixedRowColArrValue);
+
 	return 0;
 }
 
