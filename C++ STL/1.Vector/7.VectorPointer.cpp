7e2e00df2908f69d5e481ffa2ee416be029f1169
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2021-10-14 22:05:02 +0600

    c++ stl

---
+++ C++ STL/1.Vector/7.VectorPointer.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void display(vector<int> &arr) {
+	// size() methods returns the vector size
+	for (int i = 0; i < arr.size(); i++) {
+		cout << arr[i] << " ";
+	}
+	cout << endl << endl;
+}
+
+void displayPointer(vector<int> *arr) {
+	// size() methods returns the vector size
+	for (int i = 0; i < arr -> size(); i++) {
+		cout << arr -> at(i) << " ";
+	}
+	cout << endl << endl;
+}
+
+int main(int argc, char const *argv[])
+{
+	vector<int> arr;
+	arr.push_back(3);
+	arr.push_back(7);
+	arr.push_back(12);
+	arr.push_back(25);
+	arr.push_back(72);
+	arr.push_back(97);
+	arr.push_back(77);
+	arr.push_back(101);
+
+	vector<int> copyValue = arr;
+	vector<int> *copyRef = &arr;
+
+	cout << "-------------Before changes--------------" << endl;
+	cout << "------- original arr -------" <<endl;
+	display(arr);
+	cout << "------- value copy arr -------" <<endl;
+	display(copyValue);
+	cout << "------- reference copy arr -------" <<endl;
+	displayPointer(copyRef);
+
+	copyValue[3]++;
+	copyValue[4] = copyValue[3];
+
+	copyRef -> at(0) = 55;
+	copyRef -> at(1) = 55;
+	copyRef -> at(2) = 55;
+
+	cout << "-------------After changes--------------" << endl;
+	cout << "------- original arr -------" <<endl;
+	display(arr);
+	cout << "------- value copy arr -------" <<endl;
+	display(copyValue);
+	cout << "------- reference copy arr -------" <<endl;
+	displayPointer(copyRef);  
+
+	return 0;
+}
