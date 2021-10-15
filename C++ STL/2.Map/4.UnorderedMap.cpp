4e5a1df99a76d399fc739ee76cfd32280f063ecf
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2021-10-15 19:02:52 +0600

    c++ stl

---
+++ C++ STL/2.Map/4.UnorderedMap.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <string>
+#include <map>
+#include <unordered_map>
+using namespace std;
+
+void display(unordered_map<string, int> objMap) {
+	unordered_map<string, int>::iterator it;
+
+	for(it = objMap.begin(); it != objMap.end(); it++) {
+		cout << it->first << " " << it->second << endl;
+	}
+
+	cout << endl;
+}
+
+
+int main(int argc, char const *argv[])
+{
+	// declaration of map
+	unordered_map <string, int> baseMap;
+
+	//Insertion
+	baseMap.insert(pair<string, int> ("Linkon", 28));
+	baseMap.insert(make_pair("Imran", 13));
+	baseMap.insert(make_pair("Munir", 14));
+	// or easier one
+	baseMap["Nayeem"] = 17;
+
+	display(baseMap);
+
+	// Access
+	cout << "For key Imran, value is - " << baseMap.at("Imran") << endl;
+	cout << "For key Linkon, value is - " << baseMap["Linkon"] << endl;
+
+	// Modify Elements
+	baseMap.at("Imran") = 393;
+	baseMap["Nayeem"] = 115;
+	baseMap["Linkon"]++;
+
+	display(baseMap);
+
+	return 0;
+}
+
+/*
+	Unordered Map Basic
+	
+	unsorted map
+*/
