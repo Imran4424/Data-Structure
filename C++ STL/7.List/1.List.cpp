3a5b58668471012e73c7c5077d5e8534506ba0fb
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2021-10-17 00:16:23 +0600

    C++ stl

---
+++ C++ STL/7.List/1.List.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <list>
+using namespace std;
+
+
+/*
+	Lists are sequence containers that allow non-contiguous memory allocation. 
+	As compared to vector, list has slow traversal, but once a position has been found, 
+	insertion and deletion are quick. Normally, when we say a List, we talk about doubly linked list.
+*/
