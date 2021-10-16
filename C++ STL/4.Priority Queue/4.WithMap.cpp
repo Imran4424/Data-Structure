49d074bb7ff4c90021b185601b31731a5d1bae8a
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2021-10-16 10:08:47 +0600

    c++ stl

--- C++ STL/4.Priority Queue/4.WithMap.cpp
+++ C++ STL/4.Priority Queue/4.WithMap.cpp
@@ -3,6 +3,8 @@
 #include <map>
 #include <queue>
 #include <string>
+#include <stdlib.h>
+#include <time.h>
 using namespace std;
 
 void displayMaxHeap(priority_queue<int> maxHeap) {
@@ -16,9 +18,31 @@ void displayMaxHeap(priority_queue<int> maxHeap) {
 	cout << endl << endl;
 }
 
+void displayHelper(map<string, priority_queue<int> > tempList, string key) {
+	cout << key << " keeps" << endl;
+	displayMaxHeap(tempList[key]);
+}
+
 int main(int argc, char const *argv[])
 {
 	map<string, priority_queue<int> > maxHeapList;
+
+	maxHeapList["imran"] = priority_queue<int>();
+	maxHeapList["sujan"] = priority_queue<int>();
+	maxHeapList["nayeem"] = priority_queue<int>();
+
+	// Use current time as seed for random generator
+	srand(time(0));
 	
+	for (int i = 0; i < 5; i++) {
+		maxHeapList["imran"].push(rand() % 1000 + 1);
+		maxHeapList["sujan"].push(rand() % 1000 + 1);
+		maxHeapList["nayeem"].push(rand() % 1000 + 1);
+	}
+
+	displayHelper(maxHeapList, "imran");
+	displayHelper(maxHeapList, "sujan");
+	displayHelper(maxHeapList, "nayeem");
+
 	return 0;
 }
