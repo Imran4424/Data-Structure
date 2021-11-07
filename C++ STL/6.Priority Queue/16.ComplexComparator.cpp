8ff96e1526ba164e998512dcd577c507541074c2
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2021-11-07 15:44:16 +0600

    c++ stl

--- C++ STL/6.Priority Queue/16.ComplexComparator.cpp
+++ C++ STL/6.Priority Queue/16.ComplexComparator.cpp
@@ -3,8 +3,11 @@
 #include <queue>
 using namespace ::std;
 
+typedef pair<int, int> position;
+typedef pair<int, position> valuePosition;
+
 int main(int argc, char const *argv[])
 {
-
+	
 	return 0;
 }
