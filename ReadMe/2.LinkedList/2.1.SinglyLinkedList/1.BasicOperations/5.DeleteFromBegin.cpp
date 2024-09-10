e484ea2fa0696c9eb2e1a529de5787918de9f254
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2024-09-10 22:25:12 +0600

    linked list delete

---
+++ ReadMe/2.LinkedList/2.1.SinglyLinkedList/1.BasicOperations/5.DeleteFromBegin.cpp
@@ -0,0 +1,142 @@
+/*
+	5.Delete from Beginning:
+	Write a program to delete a node from the beginning of a singly linked list.
+*/
+
+#include <iostream>
+using namespace std;
+
+
+/*
+	Singly linked list have two parts
+
+	1. Data(Value)
+	2. Address - Address of next node 
+*/
+struct Node
+{
+	int data;
+	// since, this pointer will hold the next Node's address
+	// so, we need to declare Node type pointer to hold next Node's address
+	
+	// it's like, when we are trying to store int type variable's address 
+	// that time we declare a int type pointer variable
+
+	// similarly, here we need to hold Node type variable's address
+	// that's why we are declaring Node type pointer variable
+	Node* next;
+
+	Node() { }
+
+	Node(int data) {
+		this -> data = data;
+		next = NULL;
+	}
+};
+
+// global head
+// variable named head's scope is global
+// it will alive in entire code from this line
+Node* head;
+
+void insertAtEnd(int value) {
+	Node *tempNode = new Node(value);
+	
+	if (NULL == head) {
+		head = tempNode;
+		return;
+	}
+
+	// helper pointer to traverse
+	Node* travel = head;
+
+	while(NULL != travel -> next) {
+		// this statement will change the pointer's pointing to the next node
+		// why we are doing this
+		// cause we need to find the last node
+		// so we gonna do this until we find the last node
+		travel = travel -> next;
+	}
+
+	// adding the new node at the end of last node
+	travel -> next = tempNode;
+}
+
+void insertAtBegin(int value) {
+	Node *tempNode = new Node(value);
+
+	if (NULL == head) {
+		head = tempNode;
+		return;
+	}
+
+	// need to add node at the begin of linked list
+
+	// first link the whole linked list to the end of new node
+	// here, head is representing the whole current linked list
+	tempNode -> next = head;
+
+	// now change the head's node pointing to the new node
+	head = tempNode;
+}
+
+void deleteFromBegin() {
+	// linklist has no nodes
+	if (NULL == head){
+		cout << "Error! There is no node" << endl;
+		return;
+	}
+
+	// linklist has only one node
+	if (NULL == head -> next) {
+		head = NULL;
+		// cause after deleting the last node there won't be any nodes in the linked list
+	}
+
+	// linked has multiple nodes
+	// delete the first node
+	head = head -> next;
+}
+
+void display() {
+	// helper pointer to traverse
+	Node* travel = head;
+
+	while(NULL != travel) {
+		cout << travel -> data << " ";
+		// this statment will change the pointer's pointing to the next node
+		travel = travel -> next;
+	}
+
+	cout << endl;
+}
+
+int main(int argc, char const *argv[])
+{
+	head = NULL;
+
+	// this num variable's scope is the main function
+	// after main function is finished variable will be deleted or dead
+	int num;
+	cout << "How many nodes you want to insert in the linked list: ";
+	cin >> num;
+	
+	for (int i = 0; i < num; ++i) {
+		// value's scope is inside this for loop
+		// after loop is finished variable will be deleted or dead
+		int value;
+		cin >> value;
+
+		insertAtEnd(value);
+	}
+
+	// now display the linked list
+	display();
+
+	// now delete some nodes
+	deleteFromBegin();
+
+	display();
+	
+	return 0;
+}
