283350acc180e25cf5dbfca31dc9e3e2c2c5eb54
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2024-09-21 00:03:50 +0600

    another circular workaround

---
+++ ReadMe/4.Queue/4.1.QueueUsingArray/9.CircularQueueAnotherWorkAround.cpp
@@ -0,0 +1,133 @@
+/*
+	8.Struct Queue:
+	Write a program to implement a queue within struct using an array.
+*/
+
+#include <iostream>
+using namespace std;
+
+const int SIZE = 7;
+
+struct Queue
+{
+	// global array for queue
+	int queue[SIZE];
+
+	// global variable for front and rear index
+	int front = 0;
+	int rear = 0;
+
+	// these are for checking if queue is empty or not
+	// count is for tracking the queue element count
+	int count = 0;
+
+	bool isEmpty() {
+		return count == 0 ? true : false;
+	}
+
+	bool isFull() {
+		if (rear % SIZE == front) {
+			return true;
+		}
+
+		return false;
+	}
+
+	// push for queue in array
+	void push(int value) {
+		if (SIZE == rear) {
+			if(isFull()) {
+				cout << "Error!!! queue is full" << endl;
+				return;
+			}
+
+			// reseting the rear
+			rear = 0;
+		}
+
+		queue[rear] = value;
+		rear++;
+
+		// incrementing the count
+		count++;
+
+		// queue[rear++] = value;
+	}
+
+	// pop for queue in array
+	// pop operaion in queue never returns anything
+	// it just deletes the front element
+	void pop() {
+		if (SIZE == rear) {
+			if (isEmpty()) {
+				cout << "Error!!! Queue is empty" << endl;
+				return;
+			}
+
+			// resetting the front
+			front = 0;
+		}
+
+		front++;
+
+		// decrementing the count
+		count--;
+	}
+
+	// front
+	// will display the front of the queue
+	int frontOperation() {
+		if (isEmpty()) {
+			cout << "Error!!! Queue is empty" << endl;
+			return -404;
+		}
+
+		return queue[front];
+	}
+
+	// rear
+	// will display the back of the queue
+	int rearOperation() {
+		if (isEmpty()) {
+			cout << "Error!!! Queue is empty" << endl;
+			return -404;
+		}
+
+		return queue[rear - 1];
+	}
+};
+
+
+int main(int argc, char const *argv[])
+{
+	Queue pritha;
+
+	int num;
+	cout << "How many nodes you want to push in the queue: ";
+	cin >> num;
+
+	if (pritha.isEmpty()) {
+		cout << "Queue is empty" << endl;
+	}
+
+	for(int i = 0; i < num; ++i) {
+		// value's scope is inside this for loop
+		// after loop is finished variable will be deleted or dead
+		int value;
+		cin >> value;
+
+		pritha.push(value);
+		cout << "Front value is: " << pritha.frontOperation() << endl;
+		cout << "Rear value is: " << pritha.rearOperation() << endl;
+	}
+
+	pritha.pop();
+	cout << "Front value is: " << pritha.frontOperation() << endl;
+	cout << "Rear value is: " << pritha.rearOperation() << endl;
+
+	if (pritha.isEmpty()) {
+		cout << "Queue is empty" << endl;
+	}
+
+	return 0;
+}
