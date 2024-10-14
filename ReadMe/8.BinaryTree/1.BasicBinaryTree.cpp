74918a29381177c00e20decf7521841ca655eb3f
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2024-10-14 21:49:53 +0600

    binary tree

---
+++ ReadMe/8.BinaryTree/1.BasicBinaryTree.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+	int data;
+	Node* left;
+	Node* right;
+
+	Node() { }
+
+	Node(int data) {
+		this -> data = data;
+		this -> left = NULL;
+		this -> right = NULL;
+	}
+};
+
+bool isEmpty() {
+	return root == NULL ? true : false;
+}
+
+void insert()
+
+int main(int argc, char const *argv[])
+{
+	Node* root = NULL;
+	
+	return 0;
+}
